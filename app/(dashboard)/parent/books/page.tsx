4a5911e768b97df4ad1e83624a29e6208418bdb8
Robert Gant <demontegant@gmail.com>
2025-11-26 00:55:07 -0600

    opps

--- app/(dashboard)/parent/books/page.tsx
+++ app/(dashboard)/parent/books/page.tsx
@@ -41,7 +41,7 @@ export default function ParentBooksPage() {
 
       {pendingBooks.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {pendingBooks.map((book) => (
+          {pendingBooks.map((book:any) => (
             <BookApproval
               key={book.id}
               book={book}
